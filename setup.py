b23e246da3606d612ab2422676e1cbc06eaf3603
luphord <luphord@protonmail.com>
2022-04-03 22:32:36 +0200

    add dependency on pillow

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = []
+requirements = ["Pillow==9.1.0"]
 
 test_requirements = []
 
